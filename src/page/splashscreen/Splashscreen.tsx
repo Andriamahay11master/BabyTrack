4501e45c923a7236f69c6669dae3fc57551c778b
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-04 15:34:05 +0300

    add content splashscreen

--- src/page/splashscreen/Splashscreen.tsx
+++ src/page/splashscreen/Splashscreen.tsx
@@ -1,8 +1,11 @@
 import './splashscreen.scss'
 export default function Splashscreen() {
     return (
-        <div>
-            <h1>Page Splashscreen</h1>
+        <div className="splashscreen">
+            <div className="splashscreen-content">
+                <img src="/BabyTrack.jpg" alt="BabyTrack Logo" title='BabyTrack Logo'/>
+                <span className="splashscreen-title">BabyTrack</span>
+            </div>
         </div>
     )
 }
