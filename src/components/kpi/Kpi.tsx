04dfc7e1606a6c8b0bfe492059b9dca7d2e9123c
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-05 11:59:10 +0300

    add react icon to KPI

--- src/components/kpi/Kpi.tsx
+++ src/components/kpi/Kpi.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
+import { IconType } from 'react-icons';
+import * as Icons from 'react-icons/fa';
 import './kpi.scss';
 
 interface KpiProps{
+    icon ?: keyof typeof Icons
     title : string,
     value : number | string
     currency ?: string
 }
 
-export default function Kpi({title, value, currency} : KpiProps) {
+export default function Kpi({icon, title, value, currency} : KpiProps) {
+    const IconComponent:  IconType| null = icon ? Icons[icon as keyof typeof Icons] : null; // Récupérer l'icône dynamiquement
 
     return (
        <div className="kpi-item">
             <div className="kpi-col">
-                <h3 className="title-h3 kpi-title">{title}</h3>
+                {IconComponent && <IconComponent />}
             </div>
             <div className="kpi-col">
+            <h3 className="title-h3 kpi-title">{title}</h3>
                 <p className='kpi-text'>{value} {currency && <span>{currency}</span>}</p>
             </div>
        </div>
