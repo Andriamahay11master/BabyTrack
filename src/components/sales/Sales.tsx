376d84c7f44537050370ed0a1168219c5a14e2c7
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-06 12:00:54 +0300

    design dropdown select table row

--- src/components/sales/Sales.tsx
+++ src/components/sales/Sales.tsx
@@ -12,7 +12,8 @@ export default function Sales({dataList} : SalesProps) {
 
     return (
         <div className='list-block'>
-            <DataTable className='list-table' paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} stripedRows value={dataList} sortMode="multiple">
+            <DataTable className='list-table' paginator paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
+        rows={5} rowsPerPageOptions={[5, 10, 25, 50]} stripedRows value={dataList} sortMode="multiple">
                 <Column field="idsales" header="Réference" sortable></Column>
                 <Column field="description" header="Description" sortable></Column>
                 <Column field="Taille" header="Taille" sortable></Column>
