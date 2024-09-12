e7a545a9fe9e43e1a7bbd6c0149642d5db2c2082
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-12 09:05:02 +0300

    format number on table & add condition display column state

--- src/components/sales/Sales.tsx
+++ src/components/sales/Sales.tsx
@@ -2,6 +2,7 @@ import './sales.scss';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { SalesType } from '../../models/Sales';
+import { formatNumber } from '../../data/function';
         
 
 interface SalesProps {
@@ -10,6 +11,21 @@ interface SalesProps {
 
 export default function Sales({dataList} : SalesProps) {
 
+    // Fonction pour afficher "Vendu" ou "Non vendu" selon la valeur de 'etat'
+    const etatBodyTemplate = (rowData: SalesType) => {
+        return rowData.etat ? 'Vendu' : 'Non vendu';
+    };
+
+    //Fonction pour formater les prix d'achats dans le tableau
+    const formatPrixAchat = (rowData: SalesType) => {
+        return formatNumber(rowData.prixAchat.toString()) + ' MGA';
+    }
+
+    //Fonction pour formater les prix de ventes dans le tableau
+    const formatPrixVente = (rowData: SalesType) => {
+        return formatNumber(rowData.prixVente.toString()) + ' MGA';
+    }
+
     return (
         <div className='list-block'>
             <DataTable className='list-table' paginator paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
@@ -17,9 +33,9 @@ export default function Sales({dataList} : SalesProps) {
                 <Column field="idsales" header="Réference" sortable></Column>
                 <Column field="description" header="Description" sortable></Column>
                 <Column field="taille" header="Taille" sortable></Column>
-                <Column field="prixAchat" header="PrixAchat" sortable></Column>
-                <Column field="prixVente" header="PrixVente" sortable></Column>
-                <Column field="etat" header="etat" sortable></Column>
+                <Column field="prixAchat" header="PrixAchat" sortable body={formatPrixAchat}></Column>
+                <Column field="prixVente" header="PrixVente" sortable body={formatPrixVente}></Column>
+                <Column field="etat" header="Etat" sortable body={etatBodyTemplate}></Column>
             </DataTable>
         </div>
     )
