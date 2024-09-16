02d1d3480119673f2f0fdb4741422f8b50fa33be
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-16 10:01:03 +0300

    finalisation composant sign up

--- src/page/signup/SignUp.tsx
+++ src/page/signup/SignUp.tsx
@@ -5,17 +5,6 @@ import { Link } from 'react-router-dom';
 import { Poppin } from '../../components/poppin/Poppin';
 
 export default function Signup() {
-  const dataSignup = {
-    title: "Create an account",
-    formdesc: "Enter your email and password below",
-    email: "Email",
-    password: "Password",
-    passwordagain: "Confirm Password",
-    labelButton: "Sign up",
-    routeSignin: "/login",
-    textUser: "Already have an account?",
-    labelSignin: "Sign in",
-};
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -44,34 +33,34 @@ const togglePasswordConfirmationVisibility = () => {
                 <div className="logo-block">
                     <span className="logo"></span>
                 </div>
-                <h1 className="title-h1">{dataSignup.title}</h1>
-                <p className="form-desc">{dataSignup.formdesc}</p>
+                <h1 className="title-h1">Créer un compte</h1>
+                <p className="form-desc">Renseignez votre adresse email et votre mot de passe</p>
                 <div className="form-content">
                         <div className="form-group">
-                            <label htmlFor="email"><i className="icon-mail"></i>{dataSignup.email}</label>
-                            <input type="email" id="email" placeholder="Email address" value={email} onChange={ (e) => setEmail(e.target.value)}/>
+                            <label htmlFor="email"><i className="icon-mail"></i>Votre email</label>
+                            <input type="email" id="email" placeholder="Saississez votre email" value={email} onChange={ (e) => setEmail(e.target.value)}/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="password"><i className="icon-lock"></i>{dataSignup.password}</label>
+                            <label htmlFor="password"><i className="icon-lock"></i>Votre mot de passe</label>
                             <div className="input-pass">
-                                <input type={showPassword ? "text" : "password"} id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                <input type={showPassword ? "text" : "password"} id="password" placeholder="Saisissez votre mot de passe" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                 <i className={`icon ${showPassword ? "icon-eye-off" : "icon-eye"}`} onClick={togglePasswordVisibility}></i>
                             </div>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="confirmation-password"><i className="icon-lock"></i>{dataSignup.passwordagain}</label>
+                            <label htmlFor="confirmation-password"><i className="icon-lock"></i>Confirmez votre mot de passe</label>
                             <div className="input-pass">
-                                <input type={showPasswordConfirmation ? "text" : "password"} id="confirmation-password" placeholder="Confirm your Password" value={passwordAgain} onChange={(e) => setPasswordAgain(e.target.value)}/>
+                                <input type={showPasswordConfirmation ? "text" : "password"} id="confirmation-password" placeholder="Saisissez a nouveau votre mot de passe" value={passwordAgain} onChange={(e) => setPasswordAgain(e.target.value)}/>
                                 <i className={`icon ${showPasswordConfirmation ? "icon-eye-off" : "icon-eye"}`} onClick={togglePasswordConfirmationVisibility}></i>
                             </div>
                         </div>  
                         <div className="form-group form-submit">
-                            <button className={(email && password && passwordAgain) &&  (password === passwordAgain) ? "btn btn-primary" : "btn btn-primary disabled"} onClick={signup} disabled={(!email || !password || !passwordAgain) || (password !== passwordAgain)}>{dataSignup.labelButton}</button>
+                            <button className={(email && password && passwordAgain) &&  (password === passwordAgain) ? "btn btn-primary" : "btn btn-primary disabled"} onClick={signup} disabled={(!email || !password || !passwordAgain) || (password !== passwordAgain)}>Créer un compte</button>
                         </div>
-                    <p className="text-signup">{dataSignup.textUser} <Link className="btn btn-link" to={dataSignup.routeSignin}>{dataSignup.labelSignin}</Link></p>
+                    <p className="text-signup">Avez vous deja un compte ? <Link className="btn btn-link" to="/login">Se connecter</Link></p>
                 </div>
             </div>
-            {sucess && <Poppin title="Account created" message="Account created successfully" linkBtn="/login" valBtn="Sign in"/>}
+            {sucess && <Poppin title="Compte créé" message="Votre compte a été bien enregistré" linkBtn="/login" valBtn="Se connecter"/>}
         </div>
     </>
   )
