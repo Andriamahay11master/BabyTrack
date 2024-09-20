9a167678191f194d10bde8b583b7c419b216ea81
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-20 10:11:59 +0300

    add style capturePhoto

--- src/components/capture/CapturePhoto.tsx
+++ src/components/capture/CapturePhoto.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
+import './capturePhoto.scss';
 
 function CapturePhoto() {
     const webcamRef = useRef<Webcam | null>(null);
@@ -13,24 +14,26 @@ function CapturePhoto() {
 
   return (
     <div className='capturePhoto'>
-        <div className="capturePhotoTop">
-            {!capturedPhoto ? (
-                <div className='capture-col'>
-                    <Webcam
-                        audio={false}
-                        ref={webcamRef}
-                        screenshotFormat="image/jpeg"
-                    />
-                    <button className="btn btn-primary" onClick={capture}>Prendre une photo</button>
-                </div>
-            ) : (
-                <div className='capture-col'>
-                    <h3 className='title-h3'>Aperçu de votre photo:</h3>
-                    <img className="image-capture" src={capturedPhoto} alt="photo capturé" />
-                </div>
-                )}
+        <div className="captureContent">
+            <div className="capturePhotoTop">
+                {!capturedPhoto ? (
+                    <div className='capture-col'>
+                        <Webcam
+                            audio={false}
+                            ref={webcamRef}
+                            screenshotFormat="image/jpeg"
+                        />
+                        <button className="btn btn-primary" onClick={capture}>Prendre une photo</button>
+                    </div>
+                ) : (
+                    <div className='capture-col'>
+                        <h3 className='title-h3'>Aperçu de votre photo:</h3>
+                        <img className="image-capture" src={capturedPhoto} alt="photo capturé" />
+                    </div>
+                    )}
+            </div>
+            {capturedPhoto && <div className="capturePhotoAction"><button className="btn btn-primary" onClick={() => setCapturedPhoto(null)}>Prendre une nouvelle photo</button></div>}
         </div>
-        {capturedPhoto && <div className="capturePhotoAction"><button className="btn btn-primary" onClick={() => setCapturedPhoto(null)}>Prendre une nouvelle photo</button></div>}
     </div>
   );
 }
