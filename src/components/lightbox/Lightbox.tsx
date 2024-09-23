493ab69fca30621595f3febed63874efd9ef8351
Andriamahay11master <hirimanana@yahoo.fr>
2024-09-23 13:28:15 +0300

    add css on lightbox

--- src/components/lightbox/Lightbox.tsx
+++ src/components/lightbox/Lightbox.tsx
@@ -8,7 +8,7 @@ export default function Lightbox({imageSrc} : LightboxProps) {
     return (
         <div className="lightbox">
             <button className="btn btn-icon"><i className="icon-close"></i></button>
-            <div className="lightBox-image">
+            <div className="lightbox-image">
                 <img src={imageSrc} alt="Lightbox photo" title="Lightbox photo"/>
             </div>
         </div> 
